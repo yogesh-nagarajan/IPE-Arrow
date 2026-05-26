25ef2014abd6bad1351beafa236251a83e06cc1a
Ganesh Babu <ganeshbabu.g@xerago.com>
2026-05-26 17:40:31 +0530

    Implemented dummy api integration with servelt

--- core/src/main/java/com/ipearrow/aem/core/models/MultiTenantPageExporterModel.java
+++ core/src/main/java/com/ipearrow/aem/core/models/MultiTenantPageExporterModel.java
@@ -1,138 +1,138 @@
-package com.ipearrow.aem.core.models;
+// package com.ipearrow.aem.core.models;
 
-import com.adobe.cq.export.json.ComponentExporter;
-import com.adobe.cq.export.json.ExporterConstants;
-import com.day.cq.wcm.api.Page;
-import com.day.cq.wcm.api.PageManager;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.ipearrow.aem.core.services.MultiTenantExporterService;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
-import org.apache.sling.models.annotations.Exporter;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+// import com.adobe.cq.export.json.ComponentExporter;
+// import com.adobe.cq.export.json.ExporterConstants;
+// import com.day.cq.wcm.api.Page;
+// import com.day.cq.wcm.api.PageManager;
+// import com.fasterxml.jackson.annotation.JsonProperty;
+// import com.ipearrow.aem.core.services.MultiTenantExporterService;
+// import org.apache.sling.api.SlingHttpServletRequest;
+// import org.apache.sling.api.resource.Resource;
+// import org.apache.sling.api.resource.ResourceResolver;
+// import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+// import org.apache.sling.models.annotations.Exporter;
+// import org.apache.sling.models.annotations.Model;
+// import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+// import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+// import javax.annotation.PostConstruct;
+// import java.util.ArrayList;
+// import java.util.Iterator;
+// import java.util.List;
 
-@Model(
-        adaptables = SlingHttpServletRequest.class,
-        adapters = {MultiTenantPageExporterModel.class, ComponentExporter.class},
-        resourceType = {MultiTenantPageExporterModel.RESOURCE_TYPE, "ipe-arrow/components/page"},
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
-        ranking = 1500
-)
-@Exporter(
-        name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
-        extensions = ExporterConstants.SLING_MODEL_EXTENSION
-)
-public class MultiTenantPageExporterModel implements ComponentExporter {
+// @Model(
+//         adaptables = SlingHttpServletRequest.class,
+//         adapters = {MultiTenantPageExporterModel.class, ComponentExporter.class},
+//         resourceType = {MultiTenantPageExporterModel.RESOURCE_TYPE, "ipe-arrow/components/page"},
+//         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+//         ranking = 1500
+// )
+// @Exporter(
+//         name = ExporterConstants.SLING_MODEL_EXPORTER_NAME,
+//         extensions = ExporterConstants.SLING_MODEL_EXTENSION
+// )
+// public class MultiTenantPageExporterModel implements ComponentExporter {
 
-    protected static final String RESOURCE_TYPE = "ipe-arrow/components/multi-tenant-exporter";
+//     protected static final String RESOURCE_TYPE = "ipe-arrow/components/multi-tenant-exporter";
 
-    @OSGiService
-    private MultiTenantExporterService tenantService;
+//     @OSGiService
+//     private MultiTenantExporterService tenantService;
 
-    @SlingObject
-    private Resource currentResource;
+//     @SlingObject
+//     private Resource currentResource;
 
-    @SlingObject
-    private ResourceResolver resourceResolver;
+//     @SlingObject
+//     private ResourceResolver resourceResolver;
 
-    private List<PageData> pages = new ArrayList<>();
-    private boolean allowed = false;
-    private String debugMessage = "MultiTenantPageExporterModel is Active";
+//     private List<PageData> pages = new ArrayList<>();
+//     private boolean allowed = false;
+//     private String debugMessage = "MultiTenantPageExporterModel is Active";
 
-    @PostConstruct
-    protected void init() {
-        String path = currentResource.getPath();
-        if (tenantService.isAllowedPath(path)) {
-            allowed = true;
-            collectPages(path);
-        }
-    }
+//     @PostConstruct
+//     protected void init() {
+//         String path = currentResource.getPath();
+//         if (tenantService.isAllowedPath(path)) {
+//             allowed = true;
+//             collectPages(path);
+//         }
+//     }
 
-    private void collectPages(String rootPath) {
-        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
-        if (pageManager != null) {
-            Page rootPage = pageManager.getPage(rootPath);
-            if (rootPage != null) {
-                // Add the root page itself
-                pages.add(new PageData(rootPage));
+//     private void collectPages(String rootPath) {
+//         PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+//         if (pageManager != null) {
+//             Page rootPage = pageManager.getPage(rootPath);
+//             if (rootPage != null) {
+//                 // Add the root page itself
+//                 pages.add(new PageData(rootPage));
                 
-                // Recursively add all child pages
-                Iterator<Page> it = rootPage.listChildren(null, true);
-                while (it.hasNext()) {
-                    pages.add(new PageData(it.next()));
-                }
-            }
-        }
-    }
+//                 // Recursively add all child pages
+//                 Iterator<Page> it = rootPage.listChildren(null, true);
+//                 while (it.hasNext()) {
+//                     pages.add(new PageData(it.next()));
+//                 }
+//             }
+//         }
+//     }
 
-    @JsonProperty("tenantPaths")
-    public List<String> getConfiguredPaths() {
-        return tenantService.getTenantPaths();
-    }
+//     @JsonProperty("tenantPaths")
+//     public List<String> getConfiguredPaths() {
+//         return tenantService.getTenantPaths();
+//     }
 
-    @JsonProperty("isAllowed")
-    public boolean isAllowed() {
-        return allowed;
-    }
+//     @JsonProperty("isAllowed")
+//     public boolean isAllowed() {
+//         return allowed;
+//     }
 
-    @JsonProperty("pages")
-    public List<PageData> getPages() {
-        return pages;
-    }
+//     @JsonProperty("pages")
+//     public List<PageData> getPages() {
+//         return pages;
+//     }
 
-    @JsonProperty("_debug")
-    public String getDebugMessage() {
-        return debugMessage;
-    }
+//     @JsonProperty("_debug")
+//     public String getDebugMessage() {
+//         return debugMessage;
+//     }
 
-    @Override
-    public String getExportedType() {
-        return RESOURCE_TYPE;
-    }
+//     @Override
+//     public String getExportedType() {
+//         return RESOURCE_TYPE;
+//     }
 
-    /**
-     * Inner class to represent page data in JSON
-     */
-    public static class PageData {
-        private String title;
-        private String path;
-        private String name;
-        private String description;
+//     /**
+//      * Inner class to represent page data in JSON
+//      */
+//     public static class PageData {
+//         private String title;
+//         private String path;
+//         private String name;
+//         private String description;
 
-        public PageData(Page page) {
-            this.title = page.getTitle();
-            this.path = page.getPath();
-            this.name = page.getName();
-            this.description = page.getDescription();
-        }
+//         public PageData(Page page) {
+//             this.title = page.getTitle();
+//             this.path = page.getPath();
+//             this.name = page.getName();
+//             this.description = page.getDescription();
+//         }
 
-        @JsonProperty("pageTitle")
-        public String getTitle() {
-            return title;
-        }
+//         @JsonProperty("pageTitle")
+//         public String getTitle() {
+//             return title;
+//         }
 
-        @JsonProperty("pagePath")
-        public String getPath() {
-            return path;
-        }
+//         @JsonProperty("pagePath")
+//         public String getPath() {
+//             return path;
+//         }
 
-        @JsonProperty("pageName")
-        public String getName() {
-            return name;
-        }
+//         @JsonProperty("pageName")
+//         public String getName() {
+//             return name;
+//         }
 
-        @JsonProperty("pageDescription")
-        public String getDescription() {
-            return description;
-        }
-    }
-}
+//         @JsonProperty("pageDescription")
+//         public String getDescription() {
+//             return description;
+//         }
+//     }
+// }
---
+++ core/src/main/java/com/ipearrow/aem/core/servlets/ProductApiServlet.java
@@ -0,0 +1,59 @@
+package com.ipearrow.aem.core.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import org.apache.http.util.EntityUtils;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+
+import org.osgi.service.component.annotations.Component;
+
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_PATHS;
+import static org.apache.sling.api.servlets.ServletResolverConstants.SLING_SERVLET_METHODS;
+
+@Component(
+    service = Servlet.class,
+    property = {
+        SLING_SERVLET_METHODS + "=GET",
+        SLING_SERVLET_PATHS + "=/bin/productapi"
+    }
+)
+public class ProductApiServlet extends SlingSafeMethodsServlet {
+
+    @Override
+    protected void doGet(
+            SlingHttpServletRequest request,
+            SlingHttpServletResponse response)
+            throws ServletException, IOException {
+
+        String productId = request.getParameter("id");
+
+        String apiUrl ="http://localhost:3000/api/products/" + productId;
+
+        CloseableHttpClient client = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet(apiUrl);
+
+        CloseableHttpResponse apiResponse = client.execute(httpGet);
+
+        String jsonResponse = EntityUtils.toString(apiResponse.getEntity());
+
+        response.setContentType("application/json");
+
+        response.getWriter().write(jsonResponse);
+
+        apiResponse.close();
+        client.close();
+    }
+}
